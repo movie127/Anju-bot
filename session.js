e8abe4b8e5c0f1d8cf341a1e937bfb3fef0bbc46
movie127 <denuwankaveesha85@gmail.com>
2025-05-02 12:53:35 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~qRlyQQJK#J0TH5hfnvMo5vnslOOcLRYwp7J2QybVqATl4_zjkfV4",
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || "qbUjGuSuYyv2Zxh4plIp7T5UHvSt4D2g46C9",
     GITHUB_USERNAME: process.env.GITHUB_USERNAME || "movie127",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
